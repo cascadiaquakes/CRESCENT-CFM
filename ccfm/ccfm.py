cc0c7e36b6d705a36adeb66c889e0b396e467834
Richard Styron <richard.h.styron@gmail.com>
2024-03-22 09:06:42 -0700

    first build

---
+++ ccfm/ccfm.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+from .geom import make_3d_fault_mesh, make_tri_mesh
+from .cfm_io import load_cfm_traces, load_nshm_traces, write_cfm_tri_meshes
---
+++ ccfm/cfm_io.py
@@ -0,0 +1,92 @@
+import json
+
+# from typing import Optional, List
+
+
+def read_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+def write_json(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def load_cfm_traces(file_path, skip_ids=(), id_column='fid'):
+    cfm_gj = read_json(file_path)
+
+    out_features = []
+    if len(skip_ids) == 0:
+        return cfm_gj['features']
+    else:
+        for feature in cfm_gj['features']:
+            if feature[id_column] in skip_ids:
+                continue
+            out_features.append(feature)
+
+    return out_features
+
+
+def _convert_properties(properties, conversion_dict):
+
+    new_properties = {
+        key: properties.get(value, None)
+        for key, value in conversion_dict.items()
+    }
+    return new_properties
+
+
+def load_nshm_traces(file_path, skip_ids=()):
+    # TODO: Check fault traces for right-hand rule
+    nshm_conversion_dict = {
+        "id": "FaultID",
+        "name": "FaultName",
+        "dip": "DipDeg",
+        "dip_dir": "DipDir",
+        "rake": "Rake",
+        "lower_depth": "LowDepth",
+        "upper_depth": "UpDepth",
+    }
+
+    nshm_traces = load_cfm_traces(
+        file_path, skip_ids=skip_ids, id_column='FaultID'
+    )
+
+    out_features = [
+        {
+            "geometry": feature["geometry"],
+            "properties": _convert_properties(
+                feature["properties"], nshm_conversion_dict
+            ),
+            "type": "Feature",
+        }
+        for feature in nshm_traces
+    ]
+
+    return out_features
+
+
+def make_3d_tri_multipolygon(fault, tri_mesh):
+    feature = {}
+    feature['type'] = 'Feature'
+    feature['geometry'] = {
+        'type': 'MultiPolygon',
+        'coordinates': [[t] for t in tri_mesh],
+    }
+    feature['properties'] = fault['properties']
+    return feature
+
+
+def write_cfm_tri_meshes(file_path, tri_meshes, faults):
+    tri_features = [
+        make_3d_tri_multipolygon(fault, tri_mesh)
+        for fault, tri_mesh in zip(faults, tri_meshes)
+    ]
+
+    tri_mesh_gj = {
+        'type': 'FeatureCollection',
+        'features': tri_features,
+    }
+
+    write_json(file_path, tri_mesh_gj)

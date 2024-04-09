ecff7aec5e9d33dad2aab7d9f95d552aa34b506b
Richard Styron <richard.h.styron@gmail.com>
2024-04-09 14:58:26 -0700

    added draft canada faults

--- ccfm/ccfm.py
+++ ccfm/ccfm.py
@@ -1,4 +1,9 @@
 import numpy as np
 
 from .geom import make_3d_fault_mesh, make_tri_mesh
-from .cfm_io import load_cfm_traces, load_nshm_traces, write_cfm_tri_meshes
+from .cfm_io import (
+    load_cfm_traces,
+    load_nshm_traces,
+    write_cfm_tri_meshes,
+    load_canada_traces,
+)
--- ccfm/cfm_io.py
+++ ccfm/cfm_io.py
@@ -8,13 +8,12 @@ def read_json(file_path):
         return json.load(file)
 
 
-def write_json(file_path, data, minify:bool=False):
+def write_json(file_path, data, minify: bool = False):
     with open(file_path, 'w') as file:
         if minify:
-            json.dump(data, file, separators=(',',':'))
+            json.dump(data, file, separators=(',', ':'))
         else:
             json.dump(data, file, indent=2)
-        
 
 
 def load_cfm_traces(file_path, skip_ids=(), id_column='fid'):
@@ -39,6 +38,38 @@ def _convert_properties(properties, conversion_dict):
         for key, value in conversion_dict.items()
     }
     return new_properties
+
+
+def load_canada_traces(file_path, skip_ids=()):
+    canada_conversion_dict = {
+        "id": "fid",
+        "name": "name",
+        "dip": "dip",
+        "dip_dir": "dip_dir",
+        "rake": "rake",
+        "lower_depth": "lsd",
+        "upper_depth": "usd",
+    }
+
+    canada_traces = load_cfm_traces(
+        file_path, skip_ids=skip_ids, id_column='id'
+    )
+
+    out_features = [
+        {
+            "geometry": feature["geometry"],
+            "properties": _convert_properties(
+                feature["properties"], canada_conversion_dict
+            ),
+            "type": "Feature",
+        }
+        for feature in canada_traces
+    ]
+
+    for feature in out_features:
+        feature['properties']['source'] = "NRCan Faults"
+
+    return out_features
 
 
 def load_nshm_traces(file_path, skip_ids=()):
@@ -68,6 +99,9 @@ def load_nshm_traces(file_path, skip_ids=()):
         for feature in nshm_traces
     ]
 
+    for feature in out_features:
+        feature['properties']['source'] = "US NSHM"
+
     return out_features
 
 
@@ -82,7 +116,7 @@ def make_3d_tri_multipolygon(fault, tri_mesh):
     return feature
 
 
-def write_cfm_tri_meshes(file_path, tri_meshes, faults, minify:bool=False):
+def write_cfm_tri_meshes(file_path, tri_meshes, faults, minify: bool = False):
     tri_features = [
         make_3d_tri_multipolygon(fault, tri_mesh)
         for fault, tri_mesh in zip(faults, tri_meshes)
